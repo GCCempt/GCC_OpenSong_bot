b9a274dd8d5efa959c2f02a439289754d44b8648
Infosecguy10 <infosecguy10@gmail.com>
2021-04-19 20:49:47 -0400

    Update myroutine.py

--- myroutine.py
+++ myroutine.py
@@ -153,7 +153,7 @@ def main():
     #sys.exit(0)
 
     #--- ===========================
-    print('\nEnvironment Variable:', os.getenv('TOKEN'))
+    print('\nWhere are the changes going -Environment Variable:', os.getenv('TOKEN'))
     #addaffirmation()
     sys.exit(0)
 
