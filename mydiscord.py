3957f3fc294c21c35eac2fe5ec8c2cf9231cd67a
dan4222 <34949488+dan4222@users.noreply.github.com>
2021-11-27 00:49:20 -0500

    Update mydiscord.py

--- mydiscord.py
+++ mydiscord.py
@@ -37,7 +37,7 @@ READ_CHANNEL = os.environ['READCHANNELID']
 POST_CHANNEL = os.environ['POSTCHANNELID']
 set_path = 'sets/'
 bulletin_path = 'bulletin/'
-VERSION = '2.08 - Basra'
+VERSION = '2.09 - Belfast'
 
 
 def read_discord():
